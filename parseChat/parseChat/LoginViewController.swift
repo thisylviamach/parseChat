c1bcbf0d0149fa6f91de74e0693abc785e8b55cf
Sylvia Mach <sylviamach@Sylvias-MacBook-Pro.local>
2017-02-23 21:18:24 -0800

    login

--- parseChat/parseChat/LoginViewController.swift
+++ parseChat/parseChat/LoginViewController.swift
@@ -11,9 +11,17 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        signUpButton.addTarget(self, action: #selector(signUpOnClick), for: .primaryActionTriggered)
+        logInButton.addTarget(self, action: #selector(logInOnClick), for: .primaryActionTriggered)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +29,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func signUpOnClick() {
+        let user = PFUser()
+        user.username = emailTextField.text
+        user.password = passwordTextField.text
+        user.email = emailTextField.text
+        
+        user.signUpInBackground {
+            (succeeded: Bool, error: Error?) -> Void in
+            if let error = error {
+                let errorString = (error as NSError).userInfo["error"] as? NSString
+                // Show the errorString somewhere and let the user try again.
+                
+                print("signUp: \(errorString)")
+                
+            } else {
+                // Hooray! Let them use the app now.
+                print("signUp: success")
+            }
+        }
+    }
+    
+    func logInOnClick() {
+        PFUser.logInWithUsername(inBackground: emailTextField.text!, password: passwordTextField.text!) {
+            (user: PFUser?, error: Error?) -> Void in
+            if user != nil {
+                if let errorString = (error as? NSError)?.userInfo["error"] as? NSString {
+                    print("logIn: \(errorString)")
+                }
+            } else {
+                print("logIn: success")
+            }
+        }
+        
+       
+//        PFUser.logIn(withUsername: emailTextField.text!) {
+//            (user: PFUser?, error: Error?) -> Void in
+//            if let user == nil {
+//                let errorString = (error as NSerror).userInfo["error"] as? NSString
+//                
+//                print("logIn: \(errorString)")
+//            } else {
+//                
+//            }
+//        }
+        
+    }
 
 }
 
